a8ce578b89ea994a6fcf8ea0842e6c6011e6c123
Manas <tripathimanas98@gmail.com>
2020-12-27 02:40:08 +0530

    minor fixes and optimisations

--- public/parallax.js
+++ public/parallax.js
@@ -1,27 +1,4 @@
 const parallax = document.querySelectorAll("#parallax");
-
 window.addEventListener("scroll", () => {
-  let offset = window.scrollY;
-  //   console.log(ev.target.defaultView.scrollY);
-  parallax[0].style.transform = `translate3d(0px,${offset * -0.3}px,0px)`;
-  //   parallax[1].style.transform = `translate3d(0px,${
-  //     offset + offset * -0.7
-  //   }px,0px)`;
-  //   console.log(
-  //     // offset,
-  //     // parallax[0].clientHeight,
-  //     window.innerHeight,
-  //     parallax[0].offsetTop - offset,
-  //     parallax[1].offsetTop - offset,
-  //     parallax[2].offsetTop - offset
-  //   );
-  //   parallax[0].style.backgroundPositionY = offset * -0.3 + "px";
-  // parallax[1].style.backgroundPositionY = offset + offset * -0.9 + "px";
-  // parallax[2].style.backgroundPositionY = offset + offset * -0.9 + "px";
-  //   parallax.forEach((item) => {
-  //     if (item.offsetTop - offset < window.innerHeight) {
-  //       item.style.transform = `translate3d(0px,${item.offsetTop * -0.6}px,0px)`;
-  //     }
-  //     //   item.style.backgroundPositionY = offset + offset * -1.3 + "px";
-  //   });
+  parallax[0].style.transform = `translateY(${window.scrollY * -0.3}px)`;
 });
